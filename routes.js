a8149aaad0959e3fa884f6b521dffba9786bbe9f
Daniel Silva <dodosilva51@gmail.com>
2020-02-18 19:30:59 -0300

    Revert "Merge remote-tracking branch 'origin/master'"
    This reverts commit 9e82b88d039c287642da1c92a4f53379ea40ddcd.

--- routes.js
+++ routes.js
@@ -2,18 +2,10 @@ const express = require('express')
 const routes = express.Router()
 const cadastro = require('./cadastro')
 const VisitanteController = require('./src/controllers/VistanteController')
-const entrar = require('./src/controllers/usuario/entrar')
-const confirmacaoPalestra = require('./src/controllers/palestra/palestraConfirmacao')
 
 routes.get('/', function (req, res) {
-    return res.redirect('/entrar')
+    return res.redirect('/cadastro')
 })
-
-routes.get('/entrar', function (req, res) {
-    return res.render('entrar/entrar')
-});
-
-routes.post('/entrar', entrar.post);
 
 routes.get('/cadastro', function (req, res) {
     return res.render('cadastro/create')
@@ -25,11 +17,5 @@ routes.post('/cadastro', VisitanteController.store)
 routes.get('/certificado', function (req, res) {
     return res.send('certificado')
 })
-
-routes.get('/confirmacao/:palestra/:codigoAutenticacao', confirmacaoPalestra.get);
-
-routes.get('*', function (req, res) {
-    return res.render('404');
-});
 
 module.exports = routes
