04ccbbc6c3b0f5e56495e4b28f1eee8ba02d0a7c
Daniel Silva <dodosilva51@gmail.com>
2020-02-18 23:41:40 -0300

    Nota rota do da lista de certificado

--- routes.js
+++ routes.js
@@ -27,7 +27,7 @@ routes.post('/cadastro', VisitanteController.store)
 
 
 routes.get('/certificado', function (req, res) {
-    return res.send('certificado')
+    return res.render('certificado/lista');
 })
 
 routes.get('/confirmacao/:palestra/:codigoAutenticacao', confirmacaoPalestra.get);
@@ -40,7 +40,5 @@ routes.post('/palestra', PalestraController.store);
 routes.get('*', function (req, res) {
     return res.render('404');
 });
-
-
 
 module.exports = routes
