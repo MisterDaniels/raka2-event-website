a3c057f762806fa191a4a265da31423c3c8dd9a3
Douglas Scaini <41807133+douglasscaini@users.noreply.github.com>
2020-02-25 17:36:02 -0300

    Update PalestraController.js

--- src/controllers/PalestraController.js
+++ src/controllers/PalestraController.js
@@ -1,20 +1,29 @@
 const Palestra = require('../models/Palestra')
-const moment = require('moment')
+const sequelize = require('sequelize')
 
 module.exports = {
     async store(req, res) {
-        const { titulo, horario, palestrante }  = req.body
-        
-        const palestra = await Palestra.create({palestrante, titulo, horario })
+        const { titulo, horario, palestrante } = req.body
+
+        const palestra = await Palestra.create({ palestrante, titulo, horario })
 
         let message
-        if(palestra) {
+        if (palestra) {
             message = {
                 status: 'criada'
             }
         }
 
         return res.render('palestra/criarPalestra', { message })
+    },
+    async index(req, res) {
+
+        const palestras = await Palestra.findAll()
+
+        const { id } = req.params
+
+        return res.render('palestra/selecionaPalestra', { palestras, id })
+
     },
     async list(req, res) {
         const palestras = await Palestra.findAll({
@@ -25,7 +34,6 @@ module.exports = {
                 'palestrante'
             ]
         })
-        
         return res.render('palestra/mostraPalestra', { palestras })
     }
 }
